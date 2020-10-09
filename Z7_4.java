bd4f8231ea9dbe1ab2b4f2d547d492160118790d
KaloyanDochev <71563524+KaloyanDochev@users.noreply.github.com>
2020-10-09 12:11:57 +0300

    9.10

---
+++ Z7_4.java
@@ -0,0 +1,14 @@
+package Zad;
+
+public class Z7_4 {
+	public static void main (String[] args) {
+		
+		for(int i=7; i>0; i--) {
+			for(int j=0; j<i; j++) {
+		System.out.print("* ");	
+		}
+			System.out.println();	
+		}
+		
+	}
+}
