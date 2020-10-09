f2603d1c7db1f4702f40d766119eb4e30af13277
KaloyanDochev <71563524+KaloyanDochev@users.noreply.github.com>
2020-10-09 12:15:23 +0300

    Update Zad7_1.java

--- Zad7_1.java
+++ Zad7_1.java
@@ -11,7 +11,32 @@ public static void main (String[] args) {
 	System.out.println("Word 2?");
 	string2=sc.nextLine();
 	
+	int length = string1.length();
 	
+	char bukva[] = new char[length] ;
+	for(int i = 0; i<length; i++) {
+		bukva[i] = string1.charAt(i);
+	}
 	
+	boolean IsAnagram=true;
+	
+	for(int i = 0; i<length; i++) {
+		for(int j=0; j<length; j++) {
+			if(bukva[j]==string2.charAt(i)) {
+				bukva[j]=0;
+				IsAnagram=true;
+				break;
+			}
+			else {IsAnagram = false;}
+		}
+		if(IsAnagram==false) {break;}
+	}
+	
+	if(IsAnagram==true) {
+		System.out.println("Word 1 is an anagram of word 2.");
+	}
+	else {
+			System.out.println("Word 1 isn't an anagram of word 2.");
+	}
 }
 }
